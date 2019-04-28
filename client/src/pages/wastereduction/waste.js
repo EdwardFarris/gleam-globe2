72ca15599328ee0e0b316b4d92130f8a4fb1289b
Christine Jenne <44147755+cjenne@users.noreply.github.com>
2019-04-27 23:00:29 -0700

    Update waste.js

--- client/src/pages/wastereduction/waste.js
+++ client/src/pages/wastereduction/waste.js
@@ -1,24 +1,12 @@
 import React from "react";
 import "../../components/images/earthmonth.png";
-
 import './wastereduction.css';
 import wasteImg from '../../components/images/earthmonth.png';
 import Navbar from "../../components/navbar/navbar";
-
-import Card from '../../components/card/card';
-import './wastereduction.css';
-import wasteImg from '../../components/images/earthmonth.png';
-// import Navbar from "../../components/navbar/navbar";
-
-import './wastereduction.css';
-import wasteImg from '../../components/images/earthmonth.png';
-import Navbar from "../../components/navbar/navbar";
-
 
 
 export const Wastereduction = () => {
     return (
-
         <div className="overlay wasteImg" style={{ backgroundImage: `url(${wasteImg})` }}>
             <div className={`cardBackground`}>
 
@@ -37,13 +25,6 @@ export const Wastereduction = () => {
 
 
         </div>
-
-        <div className="overlay wasteImg"  style={{backgroundImage:`url(${wasteImg})`}}>
-             <Card>
-             <img src={wasteImg} alt='tips' className="tips" />
-             </Card>
-        </div> 
-
     )
 };
 export default Wastereduction;

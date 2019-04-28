6a05e8c2db6a7a8406fbe2c31c5f474bc9fba719
Christine Jenne <44147755+cjenne@users.noreply.github.com>
2019-04-27 23:16:05 -0700

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
@@ -6,11 +5,5 @@ import App from './App';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
-=======
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
->>>>>>> 02d2e16a07eb9aca8edec8f913274641d925a3fb
 

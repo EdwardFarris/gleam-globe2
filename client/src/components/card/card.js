122a8ba26163083a2d6b1ff300044071ed0df9fd
Christine Jenne <44147755+cjenne@users.noreply.github.com>
2019-04-27 22:47:10 -0700

    Update card.js

--- client/src/components/card/card.js
+++ client/src/components/card/card.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import "./card.css";
-import Navbar from '../../components/navbar/navbar';
 import Navbar from '../navbar/navbar';
-
 
 
 export const Card = (props) => (
@@ -13,27 +11,11 @@ export const Card = (props) => (
             <Navbar />
         </div>
 
-
         <div className="bg-white text-black cardtext">
             <div>{props.title}</div>
         </div>
 
         <div>{props.children}</div>
-
-        <div className="bg-dark text-white">
-            <div>
-                <div> {props.children}
-                </div>
-
-            </div>
-        </div>
-
-        <div className="bg-white text-black cardtext">
-            <div>{props.title}</div>
-        </div>
-
-        <div>{props.children}</div>
-
     </div>
 
 );

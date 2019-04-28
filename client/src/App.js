8418fa254af828a3aea7a2e37238395b5c703813
Edward Adam Farris <edwardadamfarris@Edwards-MacBook-Pro.local>
2019-04-27 22:56:30 -0700

    removed package-lock file and modified app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import logo from './logo.svg';
 import Mainpage from './pages/Mainpage/main';
 import Volunteer from './pages/volunteer/volunteer';
 import Contact from './pages/contact/contact';
